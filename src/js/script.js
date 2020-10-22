116687ecd4bdef9527eeaf992309eaf1b62f95b8
Anar <soltanovanar@mail.ru>
2020-10-22 18:00:22 +0400

    Исправлены ошибки. Добавлена логика при нажатии на кнопки. Сайт прошёл успешную валидацию.

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,11 @@
     //Modal
 
-    $('[data-modal=consultation]').on('click', function () {
-        $('.overlay, #consultation').fadeIn('slow');
-    });
-
     $('.modal__close').on('click', function () {
-        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+        $('.overlay, #order').fadeOut('slow');
     });
 
- /*    $('.button_mini').on('click', function () {
-        $('.overlay, #order').fadeIn('slow');
-    });
- */
-    $('.button_main').each(function(i) {
+    $('.main__sendBtn').each(function(i) {
         $(this).on('click', function() {
-            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         });
     });
@@ -47,24 +38,29 @@
         });
     };
 
-    validateForms('#consultation-form');
-    validateForms('#consultation form');
     validateForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
      $('form').submit(function(e) {
         e.preventDefault();
-        $.ajax({
-            type: 'POST',
-            url: 'mailer/smart.php',
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find('input').val('');
-            $('#consultation, #order').fadeOut();
-            $('.overlay, #thanks').fadeIn('slow');
-
-            $('form').trigger('reset');
-        });
         return false;
      });
+
+    const dbuttons = document.querySelectorAll('.main__dbtn');
+    const pbuttons = document.querySelectorAll('.main__pbtn');
+    
+     dbuttons.forEach(
+        item => item.addEventListener('click', function() {
+            item.classList.toggle('active');
+        })
+      );
+
+      pbuttons.forEach(
+        item => item.addEventListener('click', function() {
+            item.classList.toggle('active');
+        })
+      );
+    
+
+
